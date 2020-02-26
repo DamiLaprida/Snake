b8b03dd053b85362d2ba6b665725dd7282027076
Damian <damianfacu18@hotmail.com>
2020-02-26 02:24:47 -0300

    Snake2

---
+++ Snake2/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Snake2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.canvas = new System.Windows.Forms.PictureBox();
+            this.puntos = new System.Windows.Forms.Label();
+            this.punto = new System.Windows.Forms.Label();
+            this.bucle = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // canvas
+            // 
+            this.canvas.Location = new System.Drawing.Point(12, 12);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(750, 290);
+            this.canvas.TabIndex = 0;
+            this.canvas.TabStop = false;
+            // 
+            // puntos
+            // 
+            this.puntos.AutoSize = true;
+            this.puntos.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.puntos.Location = new System.Drawing.Point(386, 334);
+            this.puntos.Name = "puntos";
+            this.puntos.Size = new System.Drawing.Size(24, 25);
+            this.puntos.TabIndex = 1;
+            this.puntos.Text = "0";
+            // 
+            // punto
+            // 
+            this.punto.AutoSize = true;
+            this.punto.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.punto.Location = new System.Drawing.Point(295, 334);
+            this.punto.Name = "punto";
+            this.punto.Size = new System.Drawing.Size(91, 25);
+            this.punto.TabIndex = 2;
+            this.punto.Text = "Puntaje:";
+            // 
+            // bucle
+            // 
+            this.bucle.Enabled = true;
+            this.bucle.Tick += new System.EventHandler(this.bucle_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 405);
+            this.Controls.Add(this.punto);
+            this.Controls.Add(this.puntos);
+            this.Controls.Add(this.canvas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox canvas;
+        private System.Windows.Forms.Label puntos;
+        private System.Windows.Forms.Label punto;
+        private System.Windows.Forms.Timer bucle;
+    }
+}
+
